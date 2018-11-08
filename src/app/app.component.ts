460fd47844c8da7a3e93387fbae9cf50234f6436
Rodrigo Costa <rodrigo.cherubino@gmail.com>
2018-11-08 20:42:23 -0200

    initial working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  images: { id: number, imagePath: string }[] = [
+    { "id": 0, "imagePath": "assets/product-images/carro.jpg" },
+    { "id": 1, "imagePath": "assets/product-images/carro.jpg" },
+    { "id": 2, "imagePath": "assets/product-images/carro.jpg" },
+    { "id": 3, "imagePath": "assets/product-images/carro.jpg" },
+    { "id": 4, "imagePath": "assets/product-images/carro.jpg" }
+  ];
 }
